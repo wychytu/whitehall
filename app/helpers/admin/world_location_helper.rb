c912c384e2e2c1422c8a652c861135dfe9d2478f
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-06-27 11:22:26 +0100

    Remove World Location translations front end
    Remove the tabs in the admin interface that allow editing of
    translations for World Locations. Also remove the actual views displayed
    on the tab.

--- app/helpers/admin/world_location_helper.rb
+++ app/helpers/admin/world_location_helper.rb
@@ -3,12 +3,8 @@ module Admin::WorldLocationHelper
   def world_location_tabs(world_location)
     tabs = {
       "Details" => admin_world_location_path(world_location),
-      "Translations" => admin_world_location_translations_path(world_location),
-      "Features (#{Locale.new(:en).native_language_name})" => features_admin_world_location_path(world_location, locale: "")
+      "Features" => features_admin_world_location_path(world_location, locale: "")
     }
-    world_location.non_english_translated_locales.each do |locale|
-      tabs["Features (#{locale.native_language_name})"] = features_admin_world_location_path(world_location, locale: locale.code)
-    end
     tabs
   end
 end
--- test/functional/admin/world_location_translations_controller_test.rb
+++ test/functional/admin/world_location_translations_controller_test.rb
@@ -13,97 +13,9 @@ class Admin::WorldLocationTranslationsControllerTest < ActionController::TestCas
 
   should_be_an_admin_controller
 
-  view_test 'index shows a form to create missing translations' do
-    get :index, world_location_id: @location
-    translations_path = admin_world_location_translations_path(@location)
-    assert_select "form[action=?]", translations_path do
-      assert_select "select[name=translation_locale]" do
-        assert_select "option[value=fr]", text: 'Français (French)'
-        assert_select "option[value=es]", text: 'Español (Spanish)'
-      end
-
-      assert_select "input[type=submit]"
-    end
-  end
-
-  view_test 'index omits existing translations from create select' do
-    location = create(:world_location, translated_into: [:fr])
-    get :index, world_location_id: location
-    assert_select "select[name=translation_locale]" do
-      assert_select "option[value=fr]", count: 0
-    end
-  end
-
-  view_test 'index omits create form if no missing translations' do
-    location = create(:world_location, translated_into: [:fr, :es])
-    get :index, world_location_id: location
-    assert_select "select[name=translation_locale]", count: 0
-  end
-
-  view_test 'index lists existing translations' do
-    location = create(:world_location, translated_into: [:fr])
-    get :index, world_location_id: location
-    edit_translation_path = edit_admin_world_location_translation_path(location, 'fr')
-    view_location_path = world_location_path(location, locale: 'fr')
-    assert_select "a[href=?]", edit_translation_path, text: 'Français'
-    assert_select "a[href=?]", view_location_path, text: 'view'
-  end
-
-  view_test 'index does not list the english translation' do
-    get :index, world_location_id: @location
-    edit_translation_path = edit_admin_world_location_translation_path(@location, 'en')
-    assert_select "a[href=?]", edit_translation_path, text: 'en', count: 0
-  end
-
-  view_test 'index displays delete button for a translation' do
-    location = create(:world_location, translated_into: [:fr])
-    get :index, world_location_id: location
-    assert_select "form[action=?]", admin_world_location_translation_path(location, :fr) do
-      assert_select "input[type='submit'][value=?]", "Delete"
-    end
-  end
-
   test 'create redirects to edit for the chosen language' do
     post :create, world_location_id: @location, translation_locale: 'fr'
     assert_redirected_to edit_admin_world_location_translation_path(@location, id: 'fr')
-  end
-
-  view_test 'edit indicates which language is being translated to' do
-    location = create(:world_location, translated_into: [:fr])
-    get :edit, world_location_id: @location, id: 'fr'
-    assert_select "h1", text: /Edit ‘Français \(French\)’ translation/
-  end
-
-  view_test 'edit presents a form to update an existing translation' do
-    location = create(:world_location, translated_into: {fr: {name: 'Afrolasie', mission_statement: 'Enseigner aux gens comment infuser le thé'}})
-
-    get :edit, world_location_id: location, id: 'fr'
-
-    translation_path = admin_world_location_translation_path(location, 'fr')
-
-    assert_select "form[action=?]", translation_path do
-      assert_select "input[type=text][name='world_location[name]'][value='Afrolasie']"
-      assert_select "textarea[name='world_location[mission_statement]']", text: 'Enseigner aux gens comment infuser le thé'
-      assert_select "input[type=submit][value=Save]"
-    end
-  end
-
-  view_test 'edit form adds right-to-left class and dir attribute for text field and areas in right-to-left languages' do
-    location = create(:world_location, translated_into: {ar: {name: 'الناس', mission_statement: 'تعليم الناس كيفية تحضير الشاي'}})
-
-    get :edit, world_location_id: location, id: 'ar'
-
-    translation_path = admin_world_location_translation_path(location, 'ar')
-
-    assert_select "form[action=?]", translation_path do
-      assert_select "fieldset[class='right-to-left']" do
-        assert_select "input[type=text][name='world_location[name]'][dir='rtl'][value='الناس']"
-      end
-      assert_select "fieldset[class='right-to-left']" do
-        assert_select "textarea[name='world_location[mission_statement]'][dir='rtl']", text: 'تعليم الناس كيفية تحضير الشاي'
-      end
-      assert_select "input[type=submit][value=Save]"
-    end
   end
 
   view_test 'update updates translation and redirects back to the index' do
