edc595f0964a278f4ae2063c2560aeee64d1bbc1
Chris Roos <chris@seagul.co.uk>
2017-10-16 14:39:57 +0100

    Add AssetManagerFile#store
    So that we can move logic out from the AssetManagerStorage class.

--- lib/whitehall/asset_manager_storage.rb
+++ lib/whitehall/asset_manager_storage.rb
@@ -1,23 +1,8 @@
 class Whitehall::AssetManagerStorage < CarrierWave::Storage::Abstract
   def store!(file)
-    original_file = file.to_file
-    temporary_location = File.join(
-      Whitehall.asset_manager_tmp_dir,
-      SecureRandom.uuid,
-      File.basename(original_file)
-    )
-
-    FileUtils.mkdir_p(File.dirname(temporary_location))
-    FileUtils.cp(original_file, temporary_location)
-    legacy_url_path = File.join('/government/uploads', uploader.store_path)
-    AssetManagerWorker.perform_async(temporary_location, legacy_url_path)
-    file
+    path_to_file = uploader.store_path
+    AssetManagerFile.new(path_to_file).store(file, uploader)
   end
-
-  # def store!(file)
-  #   path_to_file = uploader.store_path
-  #   AssetManagerFile.new(path_to_file).store
-  # end
 
   def retrieve!(identifier)
     path_to_file = uploader.store_path(identifier)
@@ -29,10 +14,20 @@ class Whitehall::AssetManagerStorage < CarrierWave::Storage::Abstract
       @legacy_url_path = File.join('/government/uploads', path_to_file)
     end
 
-    # def store
-    #   client = GdsApi::AssetManager.new('http://localhost:3000', bearer_token: '12345678')
-    #   client.create_whitehall_asset(file: File.open('/Users/chrisroos/Desktop/wem-logo-960x640.png'), legacy_url_path: @legacy_url_path)
-    # end
+    def store(file, uploader)
+      original_file = file.to_file
+      temporary_location = File.join(
+        Whitehall.asset_manager_tmp_dir,
+        SecureRandom.uuid,
+        File.basename(original_file)
+      )
+
+      FileUtils.mkdir_p(File.dirname(temporary_location))
+      FileUtils.cp(original_file, temporary_location)
+      legacy_url_path = File.join('/government/uploads', uploader.store_path)
+      AssetManagerWorker.perform_async(temporary_location, legacy_url_path)
+      file
+    end
 
     def delete
       AssetManagerRemoveWhitehallAssetWorker.perform_async(@legacy_url_path)
