3ebc262f4a20ada3176bc7926bf99f0c62f8ed6a
Chris Roos <chris@seagul.co.uk>
2017-10-16 14:20:13 +0100

    Add composite File to wrap asset manager and quarantined file store objects
    So that we can delete from both when a file is deleted locally.

--- lib/whitehall/asset_manager_and_quarantined_file_storage.rb
+++ lib/whitehall/asset_manager_and_quarantined_file_storage.rb
@@ -5,6 +5,25 @@ class Whitehall::AssetManagerAndQuarantinedFileStorage < CarrierWave::Storage::A
   end
 
   def retrieve!(identifier)
-    Whitehall::QuarantinedFileStorage.new(uploader).retrieve!(identifier)
+    asset_manager_file = Whitehall::AssetManagerStorage.new(uploader).retrieve!(identifier)
+    sanitized_file = Whitehall::QuarantinedFileStorage.new(uploader).retrieve!(identifier)
+
+    File.new(asset_manager_file, sanitized_file)
+  end
+
+  class File
+    def initialize(asset_manager_file, sanitized_file)
+      @asset_manager_file = asset_manager_file
+      @sanitized_file = sanitized_file
+    end
+
+    def delete
+      @asset_manager_file.delete
+      @sanitized_file.delete
+    end
+
+    def url
+      @sanitized_file.url
+    end
   end
 end
